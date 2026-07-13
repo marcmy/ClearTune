a34b503eb7a1277ffbe461ac690a93de0d355b54
marcmy <21000174+marcmy@users.noreply.github.com>
2026-07-12 21:48:01 -0400

    feat: track global contrast once per tuning session

--- src/core/WizardModel.h
+++ src/core/WizardModel.h
@@ -12,7 +12,7 @@ enum class WizardPage {
     Welcome,
     Resolution,
     PixelStructure,
-    EnhancedContrast,
+    GlobalContrast,
     ClearTypeLevel,
     ContrastCombination,
     GrayscaleEnhancedContrast,
@@ -21,18 +21,24 @@ enum class WizardPage {
 
 class WizardModel {
 public:
-    explicit WizardModel(std::size_t monitorCount);
-    explicit WizardModel(std::vector<ClearTypeProfile> profiles);
+    explicit WizardModel(std::size_t monitorCount, int globalContrast = 1400);
+    explicit WizardModel(std::vector<ClearTypeProfile> profiles, int globalContrast = 1400);
 
     [[nodiscard]] WizardPage CurrentPage() const noexcept;
     [[nodiscard]] std::size_t CurrentMonitorIndex() const noexcept;
     [[nodiscard]] std::size_t MonitorCount() const noexcept;
     [[nodiscard]] bool IsSamplePage() const noexcept;
     [[nodiscard]] CalibrationStage CurrentStage() const noexcept;
+    [[nodiscard]] std::size_t CurrentSampleNumber() const noexcept;
+    [[nodiscard]] std::size_t CurrentSampleCount() const noexcept;
     [[nodiscard]] std::size_t SelectedCandidateIndex() const noexcept;
     [[nodiscard]] const ClearTypeProfile& CurrentProfile() const noexcept;
     [[nodiscard]] ClearTypeProfile& CurrentProfile() noexcept;
+    [[nodiscard]] ClearTypeProfile CurrentRenderingProfile() const noexcept;
+    [[nodiscard]] ClearTypeProfile CandidateRenderingProfile(std::size_t index) const noexcept;
     [[nodiscard]] const std::vector<ClearTypeProfile>& Profiles() const noexcept;
+    [[nodiscard]] int GlobalContrast() const noexcept;
+    [[nodiscard]] const ContrastCandidates& GlobalContrastCandidates() const noexcept;
 
     bool Next() noexcept;
     void FinishNow() noexcept;
@@ -43,6 +49,8 @@ private:
     std::vector<ClearTypeProfile> profiles_;
     std::size_t currentMonitor_{0};
     WizardPage page_{WizardPage::Welcome};
+    int globalContrast_{1400};
+    ContrastCandidates globalContrastCandidates_{};
     bool finishedFromWelcome_{false};
 };
 
